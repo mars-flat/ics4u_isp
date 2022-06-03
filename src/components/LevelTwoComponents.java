a25b6e0d55e8f410e3f51b35b432e4d336e4b4f9
mars-flat <shanec2k05@gmail.com>
2022-06-03 00:41:26 -0400

    work on question cards

--- src/components/LevelTwoComponents.java
+++ src/components/LevelTwoComponents.java
@@ -11,13 +11,14 @@ public class LevelTwoComponents extends ScreenComponent {
     private final static int TOTAL_QUESTIONS = 6;
 
     private Popup activePopup;
+    private QuestionCard currentQuestion;
     private DialoguePopup[] levelTwoDialogue;
     private QuestionCard[] questions;
-    private int currentQuestion;
+    private int curQuestionNum;
 
     public LevelTwoComponents() {
         super();
-        currentQuestion = 0;
+        curQuestionNum = 0;
         addComponents();
     }
 
@@ -40,7 +41,11 @@ public class LevelTwoComponents extends ScreenComponent {
         for (int i = 0; i < 1; ++i) {
             levelTwoDialogue[i] = new DialoguePopup(
                     new ImageView(Tools.getImage(Constants.OLDER_SIBLING, 240, 280, true, true)),
-                    "Older Sibling", dialogue[i], () -> this.setActivePopup(questions[0])
+                    "Older Sibling", dialogue[i], () ->
+            {
+                this.setActivePopup(null);
+                this.setCurrentQuestion(questions[0]);
+            }
             );
         }
 
@@ -49,9 +54,9 @@ public class LevelTwoComponents extends ScreenComponent {
     private void setupQuestions() {
 
         String[] qs = {
-                "Q: You’re greeted by a classmate you’ve never talked to before. What do you do?",
+                "Q: You're greeted by a classmate you’ve never talked to before. What do you do?",
                 "Q: You have a question about homework. What do you do?",
-                "Q: You’re talking with others in a group, and you stumble on your words. What will they most likely do?",
+                "Q: You're talking with others in a group, and you stumble on your words. What will they most likely do?",
                 "Q: You forgot your pencils for a quiz. What do you do?",
                 "Q: You see a group of people during lunch that are talking about something you like. What do you do?",
                 "Q: When faced with a presentation, you should:"
@@ -63,9 +68,12 @@ public class LevelTwoComponents extends ScreenComponent {
                         "Pretend to look at a squirrel outside",
                         "Wave or greet them back",
                         "Look at them awkwardly and give no response"},
-                2, () -> {
-
-        });
+                new String[]{"The other classmate would be sad that you ignored them...",
+                        "The other classmate would be sad that you think an imaginary squirrel is more important than them...",
+                        "That's right, a simple hi back not only resolves this crisis and could earn you a potential new friend!",
+                        "That's just going to make things more awkward than it should be."
+                },
+                2, this);
     }
 
     @Override
@@ -89,5 +97,11 @@ public class LevelTwoComponents extends ScreenComponent {
         if (activePopup != null) this.getChildren().remove(activePopup);
         activePopup = newPopup;
         if (activePopup != null) this.getChildren().add(activePopup);
+    }
+
+    public void setCurrentQuestion(QuestionCard newQuestion) {
+        if (currentQuestion != null) this.getChildren().remove(currentQuestion);
+        currentQuestion = newQuestion;
+        if (newQuestion != null) this.getChildren().add(newQuestion);
     }
 }
--- src/components/QuestionCard.java
+++ src/components/QuestionCard.java
@@ -1,6 +1,12 @@
 package components;
 
+import javafx.scene.Group;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import utilities.Constants;
 import utilities.Tools;
 
@@ -9,7 +15,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class QuestionCard extends Popup {
+public class QuestionCard extends Pane {
 
     /**
      * current todos:
@@ -21,38 +27,47 @@ public class QuestionCard extends Popup {
      *
      */
 
+    private LevelTwoComponents controller;
+
     private String question;
     private String[] options;
+    private String[] responses;
     private List<Integer> order;
 
-    private boolean flipped;
+    private Group[] cardsUp;
+    private Group[] cardsDown;
+
+    private boolean down;
     private int correctAnswer;
 
-    public QuestionCard(String question, String[] options, int correctIndex, PopupChangeRequest changeRequestHandler) {
-        super(changeRequestHandler);
+    public QuestionCard(String question, String[] options, String[] responses, int correctIndex, LevelTwoComponents parent) {
+        controller = parent;
         this.question = question;
-
-        if (options.length != 4)
-            throw new RuntimeException("Options array size invalid (must be 3)");
+        if (options.length != 4 || responses.length != 4)
+            throw new RuntimeException("Parameter array size invalid (must be 3)");
         if (correctIndex < 0 || correctIndex > 3)
             throw new RuntimeException("Correct index is invalid (must be from 0-3)");
 
         this.options = options;
+        this.responses = responses;
 
         order = new ArrayList<>(Arrays.asList(0,1,2,3));
         Collections.shuffle(order);
-        for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < 4; ++i) {
             if (correctIndex == order.get(i)) {
                 correctAnswer = i;
                 break;
             }
         }
 
-        flipped = false;
+        down = false;
         addComponents();
     }
 
     private void addComponents() {
+
+        cardsUp = new Group[4];
+        cardsDown = new Group[4];
 
         Text questionText = new Text(360, 200, question);
         questionText.setFont(Tools.getCustomFont(Constants.PRINT_FONT, 20));
@@ -61,12 +76,47 @@ public class QuestionCard extends Popup {
 
         for (int i = 0; i < 4; ++i) {
             int idx = order.get(i);
+
+            Rectangle clickBox = new Rectangle(230 + 135 * i, 370, 80, 120);
+            clickBox.setFill(Color.GRAY);
+            clickBox.setOnMouseClicked(event -> {
+                flip();
+                DialoguePopup response = new DialoguePopup(
+                        new ImageView(Tools.getImage(Constants.OLDER_SIBLING, 240, 280, true, true)),
+                        "Older Sibling", responses[idx], () -> controller.setActivePopup(null)
+                );
+
+                controller.setActivePopup(response);
+            });
+
             Text answerText = new Text(230 + 135 * i, 400, options[idx]);
             answerText.setFont(Tools.getCustomFont(Constants.PRINT_FONT, 12));
             answerText.setWrappingWidth(80);
-            this.getChildren().add(answerText);
+            answerText.setTextAlignment(TextAlignment.CENTER);
+            answerText.setPickOnBounds(false);
+            answerText.setMouseTransparent(true);
+
+            cardsUp[idx] = new Group(clickBox, answerText);
+
+            ImageView downCardImage = new ImageView(Tools.getImage((correctAnswer == i ? Constants.GOOD_CARD_DOWN : Constants.BAD_CARD_DOWN), 104, 140, true, true));
+            downCardImage.setX(220 + 135 * i);
+            downCardImage.setY(360);
+
+            cardsDown[idx] = new Group(downCardImage);
+
         }
+
+        this.getChildren().addAll(cardsUp);
+
     }
 
-
+    public void flip() {
+        if (down) {
+            // nothing for now
+        } else {
+            for (Group g : cardsUp) this.getChildren().remove(g);
+            for (Group g : cardsDown) this.getChildren().add(g);
+        }
+        down = true;
+    }
 }
